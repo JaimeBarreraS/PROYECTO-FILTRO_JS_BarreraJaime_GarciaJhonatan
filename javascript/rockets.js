8fe2360163c79a438539d053d85ebc22a8d9205b
JaimeBarreraS <Jaenba10@gmail.com>
2024-09-20 12:51:05 -0500

    :bug: API rockets

--- javascript/rockets.js
+++ javascript/rockets.js
@@ -2,12 +2,10 @@ let url = "https://api.spacexdata.com/v4/rockets";
     fetch(url)
     .then(res => res.json())
     .then(Data => {
-        Data.forEach(info => {
-            let info_rocket = document.getElementById("name");
-
-            info_rocket.innerHTML+=`
-            <h1>${info.name}</h1>
-            `
-        });
+        let info_rocket = document.getElementById("name");
+        info_rocket.innerHTML+=`
+        <h1>${Data[0].name}</h1>
+        `;
         
-    })
+    });
+        
