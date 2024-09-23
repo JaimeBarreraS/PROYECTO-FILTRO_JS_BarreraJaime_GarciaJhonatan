e618048b6b2bde887e00aa728d4b0821ea19eda1
JaimeBarreraS <Jaenba10@gmail.com>
2024-09-23 12:49:18 -0500

    :bug: Fix rockets

--- javascript/rockets.js
+++ javascript/rockets.js
@@ -4,7 +4,7 @@ fetch(url)
     .then(Data => {
         let info_rocket = document.getElementById("name");
         info_rocket.innerHTML += `
-        <h1>${Data[0].name}</h1>
+        <h1>${Data[0].name.toUpperCase()}</h1>
         `;
         let velocity_section = document.getElementById("data-velocity")
         velocity_section.innerHTML += `
@@ -35,6 +35,17 @@ fetch(url)
         let description_section = document.getElementById("description_section")
         description_section.innerHTML += `
         <p>${Data[0].description}</p>
+        `;
+        let information = document.getElementById("Information")
+        information.innerHTML += `
+        <p>Type: <span>${Data[0].type}</span></p>
+        <p>active: <span>${Data[0].active}</span></p>
+        <p>Stages: <span>${Data[0].stages}</span></p>
+        <p>Boosters: <span>${Data[0].boosters}</span></p>
+        <p>Cost Per Launch: <span>$ ${Data[0].cost_per_launch}</span></p>
+        <p>First Fling: <span>${Data[0].first_flight}</span></p>
+        <p>Country: <span>${Data[0].country}</span></p>
+        <p>Company: <span>${Data[0].company}</span></p>
         `;
     });
 
--- javascript/rockets3.js
+++ javascript/rockets3.js
@@ -4,48 +4,48 @@ fetch(url)
     .then(Data => {
         let info_rocket = document.getElementById("name");
         info_rocket.innerHTML += `
-        <h1>${Data[1].name.toUpperCase()}</h1>
+        <h1>${Data[2].name.toUpperCase()}</h1>
         `;
         let velocity_section = document.getElementById("data-velocity")
         velocity_section.innerHTML += `
-        <p>${Data[1].height.meters} Mts</p>
+        <p>${Data[2].height.meters} Mts</p>
         `;
         let velocity_section2 = document.getElementById("data-velocity2")
         velocity_section2.innerHTML += `
-        <p>${Data[1].diameter.meters} Mts</p>
+        <p>${Data[2].diameter.meters} Mts</p>
         `;
         let velocity_section3 = document.getElementById("data-velocity3")
-        let kg = Data[1].mass.kg;
+        let kg = Data[2].mass.kg;
         let tons = kg / 1000;
         velocity_section3.innerHTML += `
         <p>${tons} Tn</p>
         `;
         let velocity_section4 = document.getElementById("data-velocity4")
         velocity_section4.innerHTML += `
-        <p>${Data[1].first_stage.thrust_sea_level.kN} kN</p>
+        <p>${Data[2].first_stage.thrust_sea_level.kN} kN</p>
         `;
         let velocity_section5 = document.getElementById("data-velocity5")
         velocity_section5.innerHTML += `
-        <p>${Data[1].first_stage.thrust_vacuum.kN} kN</p>
+        <p>${Data[2].first_stage.thrust_vacuum.kN} kN</p>
         `;
         let velocity_section6 = document.getElementById("data-velocity6")
         velocity_section6.innerHTML += `
-        <p>${Data[1].second_stage.engines}</p>
+        <p>${Data[2].second_stage.engines}</p>
         `;
         let description_section = document.getElementById("description_section")
         description_section.innerHTML += `
-        <p>${Data[1].description}</p>
+        <p>${Data[2].description}</p>
         `;
         let information = document.getElementById("Information")
         information.innerHTML += `
-        <p>Type: <span>${Data[1].type}</span></p>
-        <p>active: <span>${Data[1].active}</span></p>
-        <p>Stages: <span>${Data[1].stages}</span></p>
-        <p>Boosters: <span>${Data[1].boosters}</span></p>
-        <p>Cost Per Launch: <span>$ ${Data[1].cost_per_launch}</span></p>
-        <p>First Fling: <span>${Data[1].first_flight}</span></p>
-        <p>Country: <span>${Data[1].country}</span></p>
-        <p>Company: <span>${Data[1].company}</span></p>
+        <p>Type: <span>${Data[2].type}</span></p>
+        <p>active: <span>${Data[2].active}</span></p>
+        <p>Stages: <span>${Data[2].stages}</span></p>
+        <p>Boosters: <span>${Data[2].boosters}</span></p>
+        <p>Cost Per Launch: <span>$ ${Data[2].cost_per_launch}</span></p>
+        <p>First Fling: <span>${Data[2].first_flight}</span></p>
+        <p>Country: <span>${Data[2].country}</span></p>
+        <p>Company: <span>${Data[2].company}</span></p>
         `;
     });
 
@@ -59,8 +59,8 @@ const spacexAPI = 'https://api.spacexdata.com/v4/rockets';
         const response = await fetch(spacexAPI);
         const rockets = await response.json();
     
-        if (rockets.length > 0 && rockets[1].flickr_images.length > 0) {
-            images = rockets[1].flickr_images;
+        if (rockets.length > 0 && rockets[2].flickr_images.length > 0) {
+            images = rockets[2].flickr_images;
             updateCarousel();
         }
     }
