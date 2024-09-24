cac1d5c85441c151d27fc4fde67c8a1434396b61
JaimeBarreraS <Jaenba10@gmail.com>
2024-09-23 23:06:53 -0500

    :bug: Adjustments and error repair Rockets ans Capsules

---
+++ javascript/capsulas.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("capsules-container");
+    const searchInput = document.getElementById("search");
+
+    fetch("https://api.spacexdata.com/v4/capsules")
+        .then(response => response.json())
+        .then(data => {
+            container.innerHTML = '';
+
+            const displayCapsules = (capsules) => {
+                container.innerHTML = '';
+
+                if (capsules.length === 0) {
+                    container.innerHTML = '<p>No results found</p>';
+                }
+
+                capsules.forEach(capsule => {
+                    const capsuleElement = document.createElement("div");
+                    capsuleElement.classList.add("capsule");
+
+                    if (capsule.status === 'active') {
+                        capsuleElement.classList.add("active");
+                    }
+
+                    capsuleElement.innerHTML = `
+                        <h2>${capsule.type}</h2>
+                        <p><strong>Serial:</strong> ${capsule.serial}</p>
+                        <p><strong>Status:</strong> ${capsule.status}</p>
+                        <p><strong>Original Launch:</strong> ${capsule.original_launch || "No data"}</p>
+                        <p><strong>Landings:</strong> ${capsule.land_landings}</p>
+                    `;
+
+                    container.appendChild(capsuleElement);
+                });
+            };
+
+            displayCapsules(data);
+
+            searchInput.addEventListener("input", (e) => {
+                const searchTerm = e.target.value.toLowerCase();
+                const filteredCapsules = data.filter(capsule =>
+                    capsule.serial.toLowerCase().includes(searchTerm)
+                );
+                displayCapsules(filteredCapsules);
+            });
+        })
+
+});
---
+++ javascript/rockets4.js
@@ -0,0 +1,87 @@
+let url = "https://api.spacexdata.com/v4/rockets";
+fetch(url)
+    .then(res => res.json())
+    .then(Data => {
+        let info_rocket = document.getElementById("name");
+        info_rocket.innerHTML += `
+        <h1>${Data[3].name.toUpperCase()}</h1>
+        `;
+        let velocity_section = document.getElementById("data-velocity")
+        velocity_section.innerHTML += `
+        <p>${Data[3].height.meters} Mts</p>
+        `;
+        let velocity_section2 = document.getElementById("data-velocity2")
+        velocity_section2.innerHTML += `
+        <p>${Data[3].diameter.meters} Mts</p>
+        `;
+        let velocity_section3 = document.getElementById("data-velocity3")
+        let kg = Data[3].mass.kg;
+        let tons = kg / 1000;
+        velocity_section3.innerHTML += `
+        <p>${tons} Tn</p>
+        `;
+        let velocity_section4 = document.getElementById("data-velocity4")
+        velocity_section4.innerHTML += `
+        <p>${Data[3].first_stage.thrust_sea_level.kN} kN</p>
+        `;
+        let velocity_section5 = document.getElementById("data-velocity5")
+        velocity_section5.innerHTML += `
+        <p>${Data[3].first_stage.thrust_vacuum.kN} kN</p>
+        `;
+        let velocity_section6 = document.getElementById("data-velocity6")
+        velocity_section6.innerHTML += `
+        <p>${Data[3].second_stage.engines}</p>
+        `;
+        let description_section = document.getElementById("description_section")
+        description_section.innerHTML += `
+        <p>${Data[3].description}</p>
+        `;
+        let information = document.getElementById("Information")
+        information.innerHTML += `
+        <p>Type: <span>${Data[3].type}</span></p>
+        <p>active: <span>${Data[3].active}</span></p>
+        <p>Stages: <span>${Data[3].stages}</span></p>
+        <p>Boosters: <span>${Data[3].boosters}</span></p>
+        <p>Cost Per Launch: <span>$ ${Data[3].cost_per_launch}</span></p>
+        <p>First Fling: <span>${Data[3].first_flight}</span></p>
+        <p>Country: <span>${Data[3].country}</span></p>
+        <p>Company: <span>${Data[3].company}</span></p>
+        `;
+    });
+
+
+
+const spacexAPI = 'https://api.spacexdata.com/v4/rockets';
+    let currentIndex = 0;
+    let images = [];
+    
+    async function fetchImages() {
+        const response = await fetch(spacexAPI);
+        const rockets = await response.json();
+    
+        if (rockets.length > 0 && rockets[3].flickr_images.length > 0) {
+            images = rockets[3].flickr_images;
+            updateCarousel();
+        }
+    }
+    
+    function updateCarousel() {
+        const imgElement = document.getElementById('carouselImage');
+        imgElement.src = images[currentIndex];
+    }
+    
+    const preloadImages = () => {
+        images.forEach(src => {
+            const img = new Image();
+            img.src = src;
+        });
+    };
+    
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateCarousel();
+    }, 3000); 
+    
+    preloadImages();
+    fetchImages();
+
